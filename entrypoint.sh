60942ec086b25400947b1c383db931356cee666e
Kohougen <cac31916@cac.co.jp>
2020-08-28 16:09:26 +0900

    apply checkstyle

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 echo "Running CheckStyle"
-echo "1: ${INPUT_WORKDIR}"
-echo "2: ${INPUT_CHECKSTYLE_CONFIG}"
+
 exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}"  -f xml
 
 # echo "Hello $1"
