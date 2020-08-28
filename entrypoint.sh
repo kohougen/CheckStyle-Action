129726cb93a2a6c0ba25995a9b3896c45b4b0597
Kohougen <cac31916@cac.co.jp>
2020-08-28 15:40:21 +0900

    apply checkstyle

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
-
+echo "Test0"
 echo "Running CheckStyle"
 ls
+echo "Test1"
 exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}"  -f xml
+echo "Test2"
 
 # echo "Hello $1"
 # time=$(date)
